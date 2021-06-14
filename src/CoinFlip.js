0383286376f7f9d92da99504f23fd258f44d0bc5
Connor Ashley <ashleycm@vcu.edu>
2021-06-14 18:06:12 -0400

    refactored coin imgs into an array of objects that also include side, to be passed for alt tag on child component, included flipCoin() method to setState using a callback, display appropriate coin side img based on random index from coins array, otherwise dont display anything

--- src/CoinFlip.js
+++ src/CoinFlip.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react'
-import './CoinFlip.css'
+import React, { Component } from 'react';
+import './CoinFlip.css';
+import { choice } from './helpers';
+import Coin from './Coin';
 
 class CoinFlip extends Component {
     static defaultProps = {
-        headsImg: 'https://upload.wikimedia.org/wikipedia/commons/c/cd/S_Half_Dollar_Obverse_2016.jpg',
-        tailsImg: 'https://www.usmint.gov/wordpress/wp-content/uploads/2020/12/2021-kennedy-half-dollar-uncirculated-reverse-768x768.jpg'
+        coins: [
+            {side: "heads", imgSrc: "https://upload.wikimedia.org/wikipedia/commons/c/cd/S_Half_Dollar_Obverse_2016.jpg"},
+            {side: "tails", imgSrc: "https://www.usmint.gov/wordpress/wp-content/uploads/2020/12/2021-kennedy-half-dollar-uncirculated-reverse-768x768.jpg"}
+        ]
     }
 
     constructor(props) {
@@ -18,16 +22,28 @@ class CoinFlip extends Component {
         this.handleClick = this.handleClick.bind(this);
     }
 
+    // flipCoin() {
+    //     let num = Math.floor(Math.random() * 2);
+    //     if (num === 1) {
+    //         this.setState({ side: this.props.tailsImg })
+    //     } else {
+    //         this.setState({ side: this.props.headsImg })
+    //     }
+    // }
+
     flipCoin() {
-        let num = Math.floor(Math.random() * 2);
-        if (num === 1) {
-            this.setState({ side: this.props.tailsImg })
-        } else {
-            this.setState({ side: this.props.headsImg })
-        }
+        const newCoin = choice(this.props.coins);
+        this.setState(st => {
+            return {
+                curSide: newCoin,
+                nFlips: st.nFlips + 1,
+                nHeads: st.nHeads + (newCoin.side === "heads" ? 1: 0),
+                nTails: st.nTails + (newCoin.side === "tails" ? 1: 0)
+            }
+        })
     }
 
-    handleClick() {
+    handleClick(e) {
         this.flipCoin();
     }
 
@@ -35,7 +51,9 @@ class CoinFlip extends Component {
         return (
             <div className="CoinFlip">
                 <h2>Flip A Coin!</h2>
-                <p>Out of {this.state.nFlips} flips, there have been {this.state.nHeads}</p>
+                {this.state.curSide && <Coin info={this.state.curSide} />}
+                <button onClick={this.handleClick}>Try Your Luck!</button>
+                <p>Out of {this.state.nFlips} flips, there have been {this.state.nHeads} heads and {this.state.nTails} tails</p>
             </div>
         )
     }
